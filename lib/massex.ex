4e2c137b6de4a50395c9be115201788baa3deffc
John Maxwell <john@musicglue.com>
2020-07-15 19:28:49 +0100

    feat: Massex.abs/1 Massex.compare/2

--- lib/massex.ex
+++ lib/massex.ex
@@ -7,29 +7,37 @@ defmodule Massex do
   @gram_to_ounce_rate Decimal.from_float(28.3495)
 
   @derive Jason.Encoder
-  @enforce_keys [:unit, :value]
-  defstruct [:unit, :value]
+  @enforce_keys [:unit, :amount]
+  defstruct [:unit, :amount]
 
   @type t :: %__MODULE__{
-    unit: atom(),
-    value: Decimal.t(),
-  }
+          unit: atom(),
+          amount: Decimal.t()
+        }
 
   @doc """
-  Builds a Massex struct from a value and unit
+  Builds a Massex struct from an amount and unit
 
   ## Examples
 
       iex> Massex.new(10, :gram)
-      %Massex{value: Decimal.new(10), unit: :gram}
+      %Massex{amount: Decimal.new(10), unit: :gram}
   """
-  @spec new(number() | Decimal.t() | String.t(), atom()) :: __MODULE__.t() | {:error, :invalid_value}
-  def new(value, unit) when unit in @valid_units do
-    case cast_value(value) do
-      :error -> {:error, :invalid_value}
-      val -> %__MODULE__{unit: standardize_unit(unit), value: val}
+  @spec new(number() | Decimal.t() | String.t(), atom()) ::
+          t() | {:error, :invalid_amount}
+  def new(amount, unit) when unit in @valid_units do
+    case cast_amount(amount) do
+      :error -> {:error, :invalid_amount}
+      val -> %__MODULE__{unit: standardize_unit(unit), amount: val}
     end
   end
+
+  @doc """
+  Returns a `Massex` with the arithmetical absolute of the amount
+  """
+  @spec abs(t()) :: t()
+  def abs(%__MODULE__{amount: amount, unit: unit}),
+    do: %__MODULE__{amount: Decimal.abs(amount), unit: unit}
 
   @doc """
   Adds two Massex structs together, returning a Massex
@@ -37,30 +45,69 @@ defmodule Massex do
   ## Examples
 
       iex> left = Massex.new(10, :gram)
-      ...> right = Massex.new(20, :gram)
-      ...> Massex.add(left, right)
-      %Massex{unit: :gram, value: Decimal.new(30)}
+      iex> right = Massex.new(20, :gram)
+      iex> Massex.add(left, right)
+      %Massex{unit: :gram, amount: Decimal.new(30)}
   """
-  @spec add(Massex.t(), Massex.t()) :: Massex.t()
-  def add(%__MODULE__{value: leftval, unit: unit}, %__MODULE__{value: rightval, unit: unit}), do:
-    %__MODULE__{value: Decimal.add(leftval, rightval), unit: unit}
+  @spec add(t(), t()) :: t() | {:error, :invalid_amount}
+  def add(%__MODULE__{amount: leftval, unit: unit}, %__MODULE__{amount: rightval, unit: unit}),
+    do: %__MODULE__{amount: Decimal.add(leftval, rightval), unit: unit}
 
-  def add(%__MODULE__{value: leftval, unit: leftunit}, %__MODULE__{value: rightval, unit: rightunit}), do:
-    %__MODULE__{
-      value: Decimal.add(leftval, convert_value(rightval, rightunit, leftunit)),
-      unit: leftunit,
-    }
+  def add(%__MODULE__{amount: leftval, unit: leftunit}, %__MODULE__{
+        amount: rightval,
+        unit: rightunit
+      }),
+      do: %__MODULE__{
+        amount: Decimal.add(leftval, convert_amount(rightval, rightunit, leftunit)),
+        unit: leftunit
+      }
+
+  def add(%__MODULE__{amount: amount, unit: unit}, value) do
+    case cast_amount(value) do
+      :error -> {:error, :invalid_value}
+      val -> %__MODULE__{amount: Decimal.add(amount, val), unit: unit}
+    end
+  end
+
+  @doc """
+  Compares two Massex structs, returning 0 on equality, 1 if left is greater than right, or -1 if left is less than right
+
+  ## Examples
+
+      iex> less = Massex.new(10, :gram)
+      iex> more = Massex.new(20, :gram)
+      iex> Massex.compare(less, less)
+      0
+      iex> Massex.compare(less, more)
+      -1
+      iex> Massex.compare(more, less)
+      1
+  """
+  @spec compare(t(), t()) :: integer()
+  def compare(%__MODULE__{amount: leftval, unit: unit}, %__MODULE__{amount: rightval, unit: unit}),
+    do: leftval |> Decimal.compare(rightval) |> Decimal.to_integer()
+
+  def compare(%__MODULE__{amount: leftval, unit: leftunit}, %__MODULE__{
+        amount: rightval,
+        unit: rightunit
+      }),
+      do:
+        with(
+          newval <- convert_amount(rightval, rightunit, leftunit),
+          do: leftval |> Decimal.compare(newval) |> Decimal.to_integer()
+        )
 
   defp standardize_unit(:g), do: :gram
   defp standardize_unit(:oz), do: :ounce
   defp standardize_unit(unit), do: unit
 
-  defp cast_value(%Decimal{}=value), do: value
-  defp cast_value(value) when is_float(value), do: Decimal.from_float(value)
-  defp cast_value(value) when is_number(value), do: Decimal.new(value)
-  defp cast_value(value) when is_binary(value), do:
-    with({:ok, val} <- Decimal.parse(value), do: val)
+  defp cast_amount(%Decimal{} = amount), do: amount
+  defp cast_amount(amount) when is_float(amount), do: Decimal.from_float(amount)
+  defp cast_amount(amount) when is_number(amount), do: Decimal.new(amount)
 
-  defp convert_value(value, :gram, :ounce), do: Decimal.div(value, @gram_to_ounce_rate)
-  defp convert_value(value, :ounce, :gram), do: Decimal.mult(value, @gram_to_ounce_rate)
+  defp cast_amount(amount) when is_binary(amount),
+    do: with({:ok, val} <- Decimal.parse(amount), do: val)
+
+  defp convert_amount(amount, :gram, :ounce), do: Decimal.div(amount, @gram_to_ounce_rate)
+  defp convert_amount(amount, :ounce, :gram), do: Decimal.mult(amount, @gram_to_ounce_rate)
 end
--- test/massex_test.exs
+++ test/massex_test.exs
@@ -2,28 +2,52 @@ defmodule MassexTest do
   use ExUnit.Case
   doctest Massex
 
-  test "holds a value" do
-    assert Massex.new(10, :gram).value == Decimal.new(10)
+  test "holds an amount" do
+    assert Massex.new(10, :gram).amount == Decimal.new(10)
   end
 
-  test "holds units" do
+  test "holds the unit" do
     assert Massex.new(10, :g).unit == :gram
   end
 
   test "can be Jason encoded" do
-    value = 10 |> Massex.new(:gram) |> Jason.encode!()
-    assert value == ~s({"unit":"gram","value":"10"})
+    amount = 10 |> Massex.new(:gram) |> Jason.encode!()
+    assert amount == ~s({"amount":"10","unit":"gram"})
   end
 
-  test "can be added" do
+  test "Massex.abs/1 returns the absolute amount" do
+    mass = Massex.new(-10, :gram)
+    assert Massex.abs(mass) == Massex.new(10, :gram)
+  end
+
+  test "Massex.add/2 adds two values" do
     left = Massex.new(10, :gram)
     right = Massex.new(20, :gram)
     assert Massex.add(left, right) == Massex.new(30, :gram)
   end
 
-  test "differing units standardize to left-most unit" do
+  test "Massex.add/2 standardizes to left-most unit" do
     left = Massex.new(10, :gram)
     right = Massex.new(10, :oz)
     assert Massex.add(left, right) == Massex.new("293.4950", :gram)
+  end
+
+  test "Massex.add/2 allows adding simple values to an existing Massex" do
+    mass = Massex.new(10, :gram)
+
+    assert Massex.add(mass, 10) == Massex.new(20, :gram)
+    assert Massex.add(mass, "10") == Massex.new(20, :gram)
+    assert Massex.add(mass, 10.0) == Massex.new("20.0", :gram)
+    assert Massex.add(mass, Decimal.new(10)) == Massex.new(20, :gram)
+  end
+
+  test "Massex.compare/2 compares two `Massex` structs with each other" do
+    less = Massex.new(10, :gram)
+    more = Massex.new(20, :ounce)
+
+    assert Massex.compare(less, less) == 0
+    assert Massex.compare(more, more) == 0
+    assert Massex.compare(less, more) == -1
+    assert Massex.compare(more, less) == 1
   end
 end
